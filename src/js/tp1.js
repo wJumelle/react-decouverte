ab79d2666b2667bd128e5fe725f24397b9521135
wJumelle <wjumelle@gmail.com>
2021-08-14 20:28:38 +0200

    Abandon du TP ToDoList - avancé dans la documentation nécessaire

--- src/js/tp1.js
+++ src/js/tp1.js
@@ -6,23 +6,26 @@ class ToDoList extends React.Component {
         this.state = { list: ["Toto", "Titi", "Tata"], inputValue: "" };
 
         this.addItemToList = this.addItemToList.bind(this);
+        this.updatedValue = this.updatedValue.bind(this);
     }
 
     addItemToList() {
-        this.setState( list.push(document.getElementById));
+        this.setState((state) => ({ list.push(state.inputValue) }) );
     }
 
-    updatedValue() {
-        // this.setState({inputValue: this.value });
+    updatedValue(e) {
+        const tempValue = e.target.value;
+        this.setState((state) => ({inputValue: state.inputValue + tempValue }));
+        console.log(this.state);
     }
 
     render() {
+        const listItems = this.state.list.map((todo, id) => <li key={id}>{todo}</li>);
+
         return (
             <div>
                 <ul>
-                    {this.state.list.forEach((e) => {
-                        <li>e</li>
-                    })}
+                    {listItems}
                 </ul>
                 <input id="newItem" type="text" value="" onChange={this.updatedValue} />
                 <button onClick={this.addItemToList}>Ajouter l'élément à la liste</button>
